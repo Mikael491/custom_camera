9bb4fd8e7133692ab8399c1d128fb398d78f63ad
Mikael <miketekle14@gmail.com>
2017-03-13 11:51:04 -0700

    added new vc for camera window

---
+++ custom-camera/CameraVC.swift
@@ -0,0 +1,19 @@
+//
+//  CameraVC.swift
+//  custom-camera
+//
+//  Created by Mikael Teklehaimanot on 3/13/17.
+//  Copyright © 2017 Mikael Teklehaimanot. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
