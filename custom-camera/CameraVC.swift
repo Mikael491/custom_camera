d41a455f121e24ec7b0394b9a93aa0dbf7b920ac
Mikael <miketekle14@gmail.com>
2017-03-13 12:52:47 -0700

    saved beginning of beginSession() function for switch branch

--- custom-camera/CameraVC.swift
+++ custom-camera/CameraVC.swift
@@ -7,12 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraVC: UIViewController {
+    
+    let captureSession = AVCaptureSession()
+    var previewLayer: CALayer!
+    
+    var captureDevice: AVCaptureDevice!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupCamera()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("MIKAEL: view did appear....")
+    }
+    
+    func setupCamera() {
+        
+        //capture session begins at photo screen (not video record)
+        captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+        
+        //grab devices and assign to capture device variable
+        if let availableDevices = AVCaptureDeviceDiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaTypeVideo, position: .back).devices {
+            captureDevice = availableDevices.first
+            beginSession()
+        }
+        
+    }
+    
+    func beginSession() {
+        
+        //get input from device and add to session
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(captureDeviceInput)
+        } catch {
+            print("MIKE: there was an error adding device input to session: \(error.localizedDescription)")
+        }
+        
+        //begin layer setup use session to display camera input through layer
+        if let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession) {
+            self.previewLayer = previewLayer
+            self.view.layer.addSublayer(self.previewLayer)
+            self.previewLayer.frame = self.view.layer.frame
+            captureSession.startRunning()
+            
+            
+            
+        }
         
     }
 
